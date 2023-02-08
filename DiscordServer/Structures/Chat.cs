25a998532eb37807291d654f046f53263785351a
realf <realf@DESKTOP-N8JKVAF>
2023-02-08 15:06:30 +0100

    Přidejte soubory projektu.

---
+++ DiscordServer/Structures/Chat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordServer.Structures
+{
+    public class Chat
+    {
+        public static List<Chat> Chats;
+
+        public string Name;
+        public string ID;
+        public List<Client> Clients;
+        public List<Message> Messages;
+        public ChatType ChatType;
+
+        public Chat(ChatType chatType = ChatType.PRIVATE, List<Client> clients = null)
+        {
+            if (chatType == ChatType.PRIVATE)
+                if (Chats.Any(x => x.Clients.Contains(clients[0])) || Chats.Any(x => x.Clients.Contains(clients[1])))
+                    return;
+            this.ID = Guid.NewGuid().ToString();
+            this.Clients = new List<Client>();
+            this.Messages = new List<Message>();
+            this.ChatType = chatType;
+
+        }
+
+        public void SendMessage(string message)
+        {
+            Messages.Add(new Message(message));
+        }
+    }
+
+    public class Message
+    {
+        public string MessageText;
+        public DateTime Time;
+        public Client Author;
+        
+        public Message(string message)
+        {
+            this.MessageText = message;
+            this.Time = DateTime.Now;
+        }
+    }
+
+    public enum ChatType
+    {
+        PRIVATE,GROUP
+    }
+}
---
+++ DiscordServer/Structures/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordServer.Structures
+{
+    public class Client
+    {
+        public string Password;
+        public string Name;
+        public string ID;
+
+        public DateTime Registered;
+        public List<Guid> Sessions;
+
+        public List<Chat> Chats;
+
+        //make constructor
+        public Client(string name, string password)
+        {
+            this.Name = name;
+            this.Password = Utils.CreateMD5(password);
+            this.ID = Guid.NewGuid().ToString();
+            this.Registered = DateTime.Now;
+            this.Sessions = new List<Guid>();
+            this.Chats = new List<Chat>();
+        }
+
+        public bool SendLogin()
+        {
+
+        }
+
+        public void SendMessage(Client client, string message)
+        {
+            if (Chats.Any(x => x.Clients.Contains(client)))
+            {
+                Chats.First(x => x.Clients.Contains(client)).SendMessage(message);
+            }
+            else
+            {
+                Chats.Add(new Chat(ChatType.PRIVATE, new List<Client>() { this, client }));
+                Chats.First(x => x.Clients.Contains(client)).SendMessage(message);
+            }
+        }
+
+    }
+}
